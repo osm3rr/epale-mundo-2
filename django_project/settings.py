8a8275c3152de79547e900be06188b6b9305ca77
parabavireo@gmail.com <osm3rr@gmail.com>
2022-12-22 13:26:14 -0400

    update files for deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a-@@#c4g2x3v!)ieuanq97pjp5uz*xo--21jyr9647ss^0%c-=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
